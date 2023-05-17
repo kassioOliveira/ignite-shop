87273442faaa24aa96782ad6277e9010cbef3978
kassio oliveira <kassiooliveira875@gmail.com>
2023-05-17 17:50:51 -0300

    Checkout

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -5,13 +5,13 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest,res:NextApiResponse){
 
-    const { priceId } = req.body;
+    const { items } = req.body;
 
     if (req.method !== "POST") {
       return res.status(405).json({ error: "Method not allowed." });
     }
   
-    if (!priceId) {
+    if (!items.length) {
       return res.status(400).json({ error: 'Price not found.' });
     }
 
@@ -22,12 +22,7 @@ export default async function handler(req: NextApiRequest,res:NextApiResponse){
         success_url:successUrl,
         cancel_url: cancelUrl,
         mode:'payment',
-        line_items: [
-            {
-                price: priceId,
-                quantity: 1,
-              }
-        ]
+        line_items: items
     })
 
 
