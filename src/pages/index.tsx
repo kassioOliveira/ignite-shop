93485628a3014c1831d7b2fc2e2f20cca7c92cc7
kassio oliveira <kassiooliveira875@gmail.com>
2023-05-17 09:29:45 -0300

    home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,96 @@
 
-import { styled } from '@/styles'
-import { Roboto } from 'next/font/google'
+import { HomeContainer, Product } from '@/styles/pages/home'
 
-const roboto = Roboto({weight:['400','700'] ,subsets: ['latin'] })
+import {useKeenSlider} from 'keen-slider/react'
 
-export default function Home() {
+import Image from 'next/image'
+
+import 'keen-slider/keen-slider.min.css'
+import { stripe } from '@/lib/stripe'
+import { GetStaticProps } from 'next'
+import Stripe from 'stripe'
+import Link from 'next/link'
+import Head from 'next/head'
+
+import { Tote } from 'phosphor-react'
+
+
+interface HomeProps {
+  products: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: string;
+  }[]
+}
+
+export default function Home({products}:HomeProps) {
+
+  const [sliderRef] = useKeenSlider({
+    slides:{
+      perView: 3,
+      spacing: 48
+    }
+  })
+
   return (
-   <main className={roboto.className}>
-    <h1>ola</h1>
-   </main>
+    <>
+      <Head>
+      <title> Home | Ignite Shop</title>
+      </Head>
+
+    <HomeContainer ref={sliderRef} className="keen-slider">
+
+{products.map(product => {
+  return (
+   <Link key={product.id} prefetch={false} href={`/product/${product.id}`}>
+    <Product   className='keen-slider__slide'>
+<Image src={product.imageUrl} width={520} height={480} alt='' />
+<footer>
+   <div>
+   <strong>{product.name}</strong>
+  <span>{product.price}</span>
+   </div>
+   <div>
+    <Tote size={30}  />
+   </div>
+
+</footer>
+</Product>
+   </Link>
+  )
+})}
+   </HomeContainer>
+    </>
+   
     
   )
+}
+
+export const getStaticProps:GetStaticProps =async () => {
+  const response = await stripe.products.list({
+    expand: ['data.default_price']
+  })
+
+  const products = response.data.map(product => {
+    const price = product.default_price as Stripe.Price
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: new Intl.NumberFormat('pt-BR',{
+        style:'currency',
+        currency: 'BRL'
+      }).format(price.unit_amount as number / 100)
+      
+    }
+  })
+
+  return {
+    props: {
+      products: products
+    },
+    revalidate: 60 * 60 * 1
+  }
+  
 }
---
+++ src/styles/pages/home.ts
@@ -0,0 +1,77 @@
+import { styled } from "..";
+
+
+export const HomeContainer = styled('main',{
+display:'flex',
+width:'100%',
+maxWidth: 'calc(100vw - ((100vw - 1180px) / 2) )',
+marginLeft: 'auto',
+})
+
+export const Product = styled('div',{
+    background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
+    borderRadius: 8,
+    cursor: 'pointer',
+    position: 'relative',
+    overflow: 'hidden',
+    minWidth: 540,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  
+    img: {
+      objectFit: 'cover'
+    },
+  
+    footer: {
+      position: 'absolute',
+      bottom: '0.25rem',
+      left: '0.25rem',
+      right: '0.25rem',
+      padding: '2rem',
+  
+      borderRadius: 6,
+  
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+  
+      backgroundColor: 'rgba(0, 0, 0, 0.6)',
+  
+      transform: 'translateY(110%)',
+      opacity: 0,
+      transition: 'all 0.2s ease-in-out',
+  
+     div:{
+      display:'flex',
+      flexDirection:'column',
+      gap:'0.5rem',
+      strong: {
+        fontSize: '$lg',
+        color: '$gray100'
+      },
+  
+      span: {
+        fontSize: '$xl',
+        fontWeight: 'bold',
+        color: '$green300'
+      },
+
+      
+     },
+
+     'div:nth-child(2)':{
+      backgroundColor: '$green300',
+      color:'$white',
+      padding:'0.5rem',
+      borderRadius:8
+    }
+    },
+  
+    '&:hover': {
+      footer: {
+        transform: 'translateY(0%)',
+        opacity: 1
+      }
+    }
+})
